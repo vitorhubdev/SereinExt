eeafcf25d4e460a356941bedb03a4768e666fce5
Vitor <108148783+vitorhubdev@users.noreply.github.com>
2026-09-24 15:52:35 -0300

    fix(1.0.2): initialize voice snapshot client platforms (#423)

--- crates/discord-gateway/src/voice.rs
+++ crates/discord-gateway/src/voice.rs
@@ -145,6 +145,7 @@ impl Calls {
 			status: None,
 			custom_status: None,
 			activities: vec![],
+			clients: model::ClientPlatforms::default(),
 		})
 	}
 	pub(super) fn snapshot(&self, guild: &mut GuildDto, partial: bool) -> Result<Event, Failure> {
