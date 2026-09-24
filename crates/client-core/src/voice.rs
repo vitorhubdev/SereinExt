abc57dcfa33605515bca0cfaaf4d1f7a0ef59baa
Vitor <108148783+vitorhubdev@users.noreply.github.com>
2026-09-24 15:52:31 -0300

    test(1.0.2): initialize voice member client platforms (#423)

--- crates/client-core/src/voice.rs
+++ crates/client-core/src/voice.rs
@@ -1013,6 +1013,7 @@ mod tests {
 			status: None,
 			custom_status: None,
 			activities: vec![],
+			clients: model::ClientPlatforms::default(),
 		});
 		state.apply_voice(Event::Snapshot {
 			guild: None,
